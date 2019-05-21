27385d5ee1307b339e4c435750eaa9e26eadca98
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2019-05-21 19:32:25 -0300

    adicionado item

---
+++ python_facul/notas/listas_multidim.py
@@ -0,0 +1,8 @@
+# Imprimindo valores de uma lista multidimensional em 3x3 : 
+
+arr = [[1,2,3], [4,5,6], [7,8,9]]
+
+for a in range(3):
+  for b in range(3):
+    print(arr[a][b], end=" ")
+  print()
