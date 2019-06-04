9ea5e5d94774f2b363b673a907b2ca274d6b3a45
gughog <gustavogiovanne3@gmail.com>
2019-06-04 17:41:58 -0300

    updates

---
+++ aulas_notas/aula17.py
@@ -0,0 +1,31 @@
+"""
+# Listas
+# Métodos: 
+
+.append(item) #adicionar
+.insert(index, item) #inserir
+.pop(index) ; del lista[index] #remover ; .remove(item)
+len(item) #cumprimento
+min(item) #minimo valor
+max(item) # maximo valor
+sum(item) #soma dos valores
+.sort() #ordenação
+.sort(reverse=True) #ordenação decrescente 
+.reverse() #ordenação do ultimo pro primeiro
+
+==============================================
+
+#Matrizes
+#Ex:
+
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(matriz[0])
+print(matriz[1])
+print(matriz[2])
+
+"""
---
+++ python_facul/listas/vetores_listas.py
@@ -0,0 +1,42 @@
+
+L1 = [5, 7, 2, 9, 4, 1, 3]
+L2 = [5, 7, 2, 9, 4, 1, 3]
+L2.sort()
+L3 = [5, 7, 2, 9, 4, 1, 3]
+L3.sort(reverse=True)
+# a) tamanho:
+print(
+    'a)', len(L1)
+)
+
+# b) maior valor:
+print(
+    'b)', max(L1)
+)
+
+# c) menor valor:
+print(
+    'c)', min(L1)
+)
+
+# d) soma tudo:
+print(
+    'd)', sum(L1)
+)
+
+# e) lista crescente:
+print(
+    'e)', L2
+)
+
+# f) maior valor:
+print(
+    'f)', L3
+)
+
+#2- multiplos de 3:
+
+three = list(range(0, 60, 3))
+print(
+    '2)', three
+)
